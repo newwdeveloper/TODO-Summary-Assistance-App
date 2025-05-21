0bc73d73a5cffc86e5a2b6f31dd472a8b2b4ec40
Akshay <akshaymangutkar951@gmail.com>
2025-05-21 17:38:46 +0530

    feat: add Express routes for getting, adding, and deleting todos

---
+++ backend/routes/todoRoutes.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { getTodos, addTodos, deleteTodos } from "../controllers/todoController";
+
+const router = express.Router();
+
+router.get("/todos", getTodos);
+router.post("/todos", addTodos);
+router.delete("/todos/:id", deleteTodos);
+
+export default router;
